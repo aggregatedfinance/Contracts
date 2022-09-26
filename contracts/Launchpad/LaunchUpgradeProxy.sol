52cf9c144496ec315c7502406098e9524f7b5737
agfi <team@aggregated.finance>
2022-09-26 23:36:51 +1000

    Base contracts

---
+++ contracts/Launchpad/LaunchUpgradeProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "../@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract LaunchUpgradeProxy is TransparentUpgradeableProxy {
+
+    constructor(address admin, address logic, bytes memory data) TransparentUpgradeableProxy(logic, admin, data) {
+
+    }
+}
